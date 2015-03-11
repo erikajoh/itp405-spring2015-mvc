9e45cfc4d1878c75899cab0b9bf4e802df61ca75
Erika Marie Johnson <erikamjoh@gmail.com>
2015-03-11 00:18:20 -0700

    updated look

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <title>The DVD App</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light' rel='stylesheet' type='text/css'>
     <style>
 
 	    body {
 	    	display: block;
 			font-family: 'Avenir';
-			background-color: #FBFBF8;
+			background-color: #fff;
 			color: #008195;
 		}
 
@@ -27,36 +28,69 @@
 			color: #C01A36;
 		}
 
-		.flowers-right {
-			height:100vh;
-			right:0;
-			float:right;
-			position:fixed;
-		}
-
-		.flowers-left {
-			height:100vh;
-			left:0;
-			float:left;
-			position:fixed;
-			-moz-transform: scaleX(-1);
-	        -o-transform: scaleX(-1);
-	        -webkit-transform: scaleX(-1);
-	        transform: scaleX(-1);
-	        filter: FlipH;
-	        -ms-filter: 'FlipH';
-		}
-
 		.btn {
 			color: #C01A36;
+		}
+
+		.nav {
+			padding-top: 10px;
+			width: 100%;
+			background: #C01A36;
+			color: white;
+		}
+
+		.nav a {
+			color: #fff;
+			padding: 5px 10px;
+		}
+
+		.right {
+			float: right;
+			margin-right: 40px;
+		}
+
+		.left {
+			float: left;
+			margin-left: 40px;
+		}
+
+		p {
+			font-family: 'Avenir';
+			font-size: 14px;
+		}
+
+		.container {
+			width: 100%;
+		}
+
+		.title {
+			font-size: 68px;
+			margin-bottom: 20px;
+			font-family: 'Shadows Into Light';
+			color: white;
 		}
 
     </style>
 </head>
 <body>
 
-<img src="{{asset('assets/flowers.png')}}" class="flowers-left">
-<img src="{{asset('assets/flowers.png')}}" class="flowers-right">
+<div class="nav">
+	<span class="left">
+		<p>
+			THE DVD APP
+		</p>
+	</span>
+	<span class="right">
+		<p>
+			<a href="/">home</a> &nbsp;
+			<a href="/dvds/search">search</a> &nbsp;
+			<a href="/dvds/create">create</a> &nbsp;
+			<a href="/dvds">view all</a> &nbsp;
+			<a href="/about">about</a>
+		</p>
+	</span>
+</div>
+
 <div class="container">
   @yield('content')
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,97 +1,52 @@
-<html>
-	<head>
-		<link href='//fonts.googleapis.com/css?family=Lato:100,200,300' rel='stylesheet' type='text/css'>
-    	<meta charset="UTF-8">
-    	<title>The DVD App</title>
-		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				display: table;
-				font-weight: 200;
-				font-family: 'Avenir';
-				background-color: #FBFBF8;
-				color: #008195;
-			}
+@extends('layout')
 
-			p {
-				font-family: 'Avenir';
-				font-size: 14px;
-				color: #008195;
-			}
+@section('content')
 
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
+<style>
+	body {
+		background: #C01A36;
+		color: #fff;
+	}
 
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
+	.welcome {
+		text-align: center;
+		display: table;
+		vertical-align: middle;
+		margin: 30vh auto;
+	}
 
-			.title {
-				font-size: 96px;
-				margin-bottom: 20px;
-			}
+	.quote {
+		margin-bottom: 80px;
+		color: #fff;
+	}
 
-			.quote {
-				font-size: 24px;
-				margin-bottom: 80px;
-				color: #008195;
-				font-family: 'Avenir Next';
-			}
+	a, a:link, a:hover, a:visited, a:active {
+		color: #fff;
+	}
 
-			a, a:link, a:visited, a:active {
-				color: #C01A36;
-				text-decoration: none;
-			}
+	.nav {
+		background: #fff;
+		color: #008195;
+	}
 
-			a:hover {
-				color: #C01A36;
-				text-decoration: underline;
-			}
+	.nav a {
+		color: #008195;
+	}
 
-			.flowers-right {
-				height:100vh;
-				right:0;
-				float:right;
-				position:fixed;
-			}
+</style>
 
-			.flowers-left {
-				height:100vh;
-				left:0;
-				float:left;
-				position:fixed;
-				-moz-transform: scaleX(-1);
-		        -o-transform: scaleX(-1);
-		        -webkit-transform: scaleX(-1);
-		        transform: scaleX(-1);
-		        filter: FlipH;
-		        -ms-filter: 'FlipH';
-			}
+<div class="welcome">
+	<div class="title">
+		THE DVD APP
+	</div>
 
-		</style>
-	</head>
-	<body>
-		<img src="{{asset('assets/flowers.png')}}" class="flowers-left">
-		<div class="container">
+	<div class="quote">DVDs por favor :)</div>
 
-			<div class="title">
-				<!-- <img src="{{asset('assets/dvd.png')}}" width="10%"> -->
-				<!-- <br> -->
-				THE DVD APP
-			</div>
-			<div class="quote">DVDs por favor :)</div>
-			<img src="{{asset('assets/dvd.png')}}" width="10%">
-			<br><br>
-			<p><a href="dvds/search">search</a> &nbsp; // &nbsp; <a href="dvds/create">create</a> &nbsp; // &nbsp; <a href="dvds">view all</a></p>
+	<br><br>
 
-		</div>
-		<img src="{{asset('assets/flowers.png')}}" class="flowers-right">
-	</body>
-</html>
+	<p><a href="dvds/search">search</a> &nbsp; // &nbsp; <a href="dvds/create">create</a> &nbsp; // &nbsp; <a href="dvds">view all</a></p>
+
+	</div>
+</div>
+
+@stop
